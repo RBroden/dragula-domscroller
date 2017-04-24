b79af2d2a96965107e5e2c82e9622e2e7c1e0f90
RBroden <robertbroden@outlook.com>
2017-04-23 23:29:21 -0400

    second commit

---
+++ demo/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'example-app',
+  template: `
+    <div class="examples">
+
+      <my-example></my-example>
+
+      <example-a></example-a>
+    
+      <example-b></example-b>
+    
+      <another-example></another-example>
+    
+      <such-example></such-example>
+    
+      <very-example></very-example>
+    
+      <much-example></much-example>
+    
+      <wow-example></wow-example>
+    
+      <repeat-example></repeat-example>
+    
+      <nested-repeat-example></nested-repeat-example>
+    </div>
+  `,
+  encapsulation: ViewEncapsulation.None
+})
+export class DemoComponent {
+}
